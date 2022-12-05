2a5af468f5277a44e69ef70649f4d798f3538bd2
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2022-12-04 19:02:34 -0500

    add template file and updates.

--- Backgrounds/run.sh
+++ Backgrounds/run.sh
@@ -1,4 +1,9 @@
 
+xelatex template_background.beamer.tex & \ 
+	convert -density 300 template_background.pdf template_background.png
 
 xelatex background001.beamer.tex & \ 
 	convert -density 1200 background001.beamer.pdf background001.png
+
+xelatex background002.beamer.tex & \ 
+	convert -density 1200 background002.beamer.pdf background002.png
